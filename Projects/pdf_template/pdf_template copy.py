092ced9bb52fa78c60fb9591ad9ec31cb186fb47
diegofaco <df.diegofaco@gmail.com>
2023-06-16 19:27:27 -0300

    asasas

--- Projects/pdf_template/pdf_template copy.py
+++ Projects/pdf_template/pdf_template copy.py
@@ -5,19 +5,11 @@ from reportlab.platypus import Table, TableStyle, Paragraph
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.utils import ImageReader
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-import matplotlib.pyplot as plt
-
-# CB: 1.1 - Register Helvetica font
-pdfmetrics.registerFont(TTFont('Helvetica', 'Helvetica.ttf'))
 
 # CB: 2.0 - Define common elements
 def create_element(c, text, x, y, font, size, color):
     c.setFont(font, size)
-    if not isinstance(color, colors.Color):
-        color = colors.Color(*color)  # Create a Color object from the tuple
-    c.setFillColorRGB(color.red, color.green, color.blue)
+    c.setFillColorRGB(*color)
     c.drawString(x, y, text)
 
 def create_header(c, text, x=30, y=750):
@@ -42,14 +34,12 @@ def format_body_text(c, text, x=30, y=700):
     p.wrapOn(c, 500, y)
     p.drawOn(c, x, y)
 
-def format_bullet_points(c, points, x, y, width, style, indent=0):
+def format_bullet_points(c, points, x, y, width, style):
     line_height = style.leading
     for i, point in enumerate(points):
         y_point = y - i * line_height
-        if isinstance(point, list):
-            format_bullet_points(c, point, x + 20, y_point, width, style, indent + 1)
-        else:
-            create_element(c, "•" * indent + " " + point, x, y_point, style.fontName, style.fontSize, style.textColor)
+        create_element(c, "•", x, y_point, style.fontName, style.fontSize, style.textColor)
+        create_element(c, point, x + 20, y_point, style.fontName, style.fontSize, style.textColor)
 
 def format_table(c, data, x, y, style):
     if data is None or not all(isinstance(row, list) for row in data):
@@ -62,30 +52,12 @@ def format_table(c, data, x, y, style):
 def format_image(c, path, x, y, width):
     img = ImageReader(path)
     img_width, img_height = img.getSize()
-    height = width * img_height /img_width
+    height = width * img_height / img_width
     c.drawImage(path, x, y-height, width, height)
 
-# CB: 4.0 - Create bar chart
-def create_bar_chart(data, labels, title, filename):
-    fig, ax = plt.subplots()
-    ax.bar(labels, data)
-    ax.set_title(title)
-    fig.savefig(filename)
-
-# CB: 5.0 - Create watermark
-def create_watermark(c, text, x=300, y=400):
-    c.saveState()
-    c.setFont("Helvetica", 80)
-    c.setFillColorRGB(0.85, 0.85, 0.85)  # Light grey
-    c.rotate(45)
-    c.drawCentredString(x, y, text)
-    c.restoreState()
-
-# CB: 6.0 - Main function
+# CB: 4.0 - Main function
 def create_pdf(report):
     c = canvas.Canvas("report.pdf", pagesize=A4)
-    c.setTitle(report['title'])
-    c.setAuthor(report['author'])
     for i in range(len(report["headings"])):
         create_header(c, report["header"])
         create_footer(c, report["footer"])
@@ -93,10 +65,9 @@ def create_pdf(report):
         format_heading(c, report["headings"][i])
         format_subheading(c, report["subheadings"][i])
         format_body_text(c, report["body_text"][i])
-        style = ParagraphStyle('BulletPoints', parent=getSampleStyleSheet()['BodyText'], fontName='Helvetica', fontSize=10, textColor=colors.black, leftIndent=20, leading=12)
+        style = ParagraphStyle('BulletPoints', parent=getSampleStyleSheet()['BodyText'], fontName='Courier', fontSize=10, textColor=colors.black, leftIndent=20, leading=12)
         format_bullet_points(c, report["bullet_points"][i], 30, 640, 500, style)
-        create_bar_chart([10, 15, 7, 10], ['Q1', 'Q2', 'Q3', 'Q4'], 'Sales', 'chart.png')
-        format_image(c, 'chart.png', 30, 600, 200)
+        format_image(c, report["images"][i], 30, 600, 200)
         style = TableStyle([
             ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
@@ -108,26 +79,21 @@ def create_pdf(report):
             ('GRID', (0,0), (-1,-1), 1, colors.black)
         ])
         format_table(c, report["tables"][i], 30, 200, style)
-        create_watermark(c, 'DRAFT')
-        if i < len(report["headings"]) - 1 and report['page_break'][i]:
-            c.showPage()
+        c.showPage()
     c.save()
 
-# CB: 7.0 - Test the template
+# CB: 5.0 - Test the template
 def test_template():
     report = {
         "header": "My Report Header",
         "footer": "Page 1",
         "headings": ["Main Heading", "Another Heading"],
         "subheadings": ["Subheading", "Another Subheading"],
-        "body_text": ["This is some more body text that can be elaborated upon to provide additional information and context. As we delve further into the topic, it becomes apparent that there are various aspects to consider and explore. By expanding upon this body text, we can delve into the intricacies, nuances, andrelated subject matter, shedding light on the matter at hand.", "This is some more body text."],
+        "body_text": ["This is some more body text that can be elaborated upon to provide additional information and context. As we delve further into the topic, it becomes apparent that there are various aspects to consider and explore. By expanding upon this body text, we can delve into the intricacies, nuances, and related subject matter, shedding light on the matter at hand.", "This is some more body text."],
         "bullet_points": [["Point 1", "Point 2", "Point 3"], ["Another Point 1", "Another Point 2"]],
         "images": ["C:\\github\\ia\\Projects\\pdf_template\\grafico01.jpg", "C:\\github\\ia\\Projects\\pdf_template\\grafico02.jpg"],
         "tables": [[["Header 1", "Header 2"], ["Row 1, Column 1", "Row 1, Column 2"], ["Row 2, Column 1", "Row 2, Column 2"]], None],
-        "page_numbers": [1, 2],
-        "page_break": [True, False],
-        "title": "My Report",
-        "author": "Paul"
+        "page_numbers": [1, 2]
     }
     create_pdf(report)
 
